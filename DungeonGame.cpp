a6d78a61a72c87c44d37dca613bdde877ea5a777
KirstyWalesDesktop <kirsty.eleanor.evans@gmail.com>
2022-01-31 17:06:13 +0000

    Removed old comments

--- DungeonGame.cpp
+++ DungeonGame.cpp
@@ -5,8 +5,6 @@ std::string charName;
 
 void introduction() {
     std::cout << "Please enter your character's name:\n";
-
-    //std::cin.getline(charName);
     std::getline(std::cin, charName);
     std::cout << "Well, shit on my nipples " << charName
          << " and call me a gnoll, you'd better get in there and clap some "

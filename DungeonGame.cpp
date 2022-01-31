0adf1f5bf5622bedf41178f2d417972f8ec84e40
KirstyWalesDesktop <kirsty.eleanor.evans@gmail.com>
2022-01-31 16:25:04 +0000

    fixed my garbage while loop and took my introduction function out of main

--- DungeonGame.cpp
+++ DungeonGame.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
 using namespace std;
 
-int DungeonGame () {
-    cout << "Hello There";
-}
+bool isAlive = true;
+char charName[50];
 
+void introduction()
+    {
+        cout << "Please enter your character's name:\n";
+
+        cin.getline(charName, 50);
+        cout << "Well, shit on my nipples " << charName << " and call me a gnoll, you'd better get in there and clap some goblin cheeks.\n";
+    }
+
+int main()
+{
+    while (isAlive == true) {
+        introduction();
+
+        
+            
+        }
+    
+}

be7b8894c8766c9e8f4aede6edcfd247598e80cb
KirstyWalesDesktop <kirsty.eleanor.evans@gmail.com>
2022-02-03 22:37:30 +0000

    Add introduction

--- DungeonGame.cpp
+++ DungeonGame.cpp
@@ -1,11 +1,37 @@
 #include "DungeonGame.hpp"
+
 #include "Room.hpp"
 
 bool isAlive = true;
 
+void introduction() {
+    bool validCharName = false;
+
+outer:
+    while (validCharName = false) {
+        std::cout << "Please enter your name\n";
+        std::cin >> charName;
+        std::cout << "The name you have entered is " << charName
+                  << " is this correct?";
+
+        char c;
+
+        while (true) {
+            std::cout << "Please enter Y or N";
+            std::cin >> c;
+            if (c == 'n') {
+                goto outer;
+            } else if (c == 'y') {
+                validCharName = true;
+                break;
+            }
+        }
+    }
+}
+
 int main() {
     while (isAlive == true) {
-        Room entrance;
+        introduction();
 
         likeToDo();
     }
